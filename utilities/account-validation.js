80c6f17cfbe7f4e7d092d087476a458c4acca6d5
maheye <u277721157@byui.edu>
2024-02-22 11:38:58 +0800

    week 04 team

--- utilities/account-validation.js
+++ utilities/account-validation.js
@@ -1,6 +1,8 @@
 const utilities = require(".")
 const { body, validationResult } = require("express-validator")
 const validate = {}
+const accountModel = require("../models/account-model")
+
 
 /*  **********************************
  *  Registration Data Validation Rules
@@ -19,12 +21,18 @@ validate.registationRules = () => {
         .isLength({ min: 2 })
         .withMessage("Please provide a last name."), // on error this message is sent.
   
-      // valid email is required and cannot already exist in the DB
+      // valid email is required and cannot already exist in the database
       body("account_email")
-      .trim()
-      .isEmail()
-      .normalizeEmail() // refer to validator.js docs
-      .withMessage("A valid email is required."),
+        .trim()
+        .isEmail()
+        .normalizeEmail() // refer to validator.js docs
+        .withMessage("A valid email is required.")
+        .custom(async (account_email) => {
+          const emailExists = await accountModel.checkExistingEmail(account_email)
+          if (emailExists){
+            throw new Error("Email exists. Please log in or use different email")
+          }
+        }),
   
       // password is required and must be strong password
       body("account_password")
